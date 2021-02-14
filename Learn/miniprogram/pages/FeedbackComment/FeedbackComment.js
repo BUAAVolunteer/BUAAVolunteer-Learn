074e12a14004849c19441894a2bd78e3fe95c69e
ShouHou <18373337@buaa.edu.cn>
2021-02-14 19:44:01 +0800

    回复反馈页面

--- Learn/miniprogram/pages/FeedbackComment/FeedbackComment.js
+++ Learn/miniprogram/pages/FeedbackComment/FeedbackComment.js
@@ -1,66 +1,48 @@
-// miniprogram/pages/FeedbackComment/FeedbackComment.js
-Page({
+// pages/Admin/FeedbackComment/FeedbackComment.js
+const db = wx.cloud.database();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
 
   /**
-   * 页面的初始数据
+   * 组件的初始数据
    */
   data: {
-
+    comment_list: [],
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 组件的方法列表
    */
-  onLoad: function (options) {
-
+  methods: {
+    deleteItem(e) {
+      let index = parseInt(e.currentTarget.id);
+      let _id = this.data.comment_list[index]._id;
+      db.collection("feedback")
+        .doc(_id)
+        .remove()
+        .then(
+          this.setData({
+            comment_list: this.data.comment_list.splice(index, 1),
+          })
+        );
+    },
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 组件的生命周期
    */
-  onReady: function () {
-
+  lifetimes: {
+    created() {
+      db.collection("feedback")
+        .get()
+        .then((e) => {
+          console.log(e.data)
+          this.setData({
+            comment_list: e.data,
+          });
+        });
+    },
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
